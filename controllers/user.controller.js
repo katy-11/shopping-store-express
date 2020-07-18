3079c0ab84c6269b7f668ea264be50d603cbeccf
huyenho <10hohuyen1114@gmail.com>
2020-07-18 21:45:20 +1000

    fix mail sendgrid

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -62,7 +62,7 @@ module.exports.cookie = (req, res) => {
     html: "<strong>and easy to do anywhere, even with Node.js</strong>"
   };
   sgMail.send(msg);
-  res.redirect("/transactions/userTransaction");
+  res.redirect("/users");
 };
 
 module.exports.postCreate = (req, res) => {
