bce2a67d763c360ffed741b256f15801fba6fbfb
huyenho <10hohuyen1114@gmail.com>
2020-09-03 18:34:08 +1000

    unnecessary file

--- db.js
+++
@@ -1,9 +0,0 @@
-var low = require("lowdb");
-var FileSync = require("lowdb/adapters/FileSync");
-
-var adapter = new FileSync("db.json");
-var db = low(adapter);
-
-db.defaults({ books: [], users: [], sessions: [] }).write();
-
-module.exports = db;
